a30e7747af7e9e2c7dda0d5a1dfc7cb7a7bf04fc
cody-junier <codyjunier@gmail.com>
2022-02-26 12:42:57 -0800

    More heroku!

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+const dotenv = require('dotenv')
+require('dotenv').config()
+console.log(process.env.test)
+console.log(process.env.MONGODB_URI2)
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = (process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true,
-useUnifiedTopology: true
-});
-
+const MONGODB_URI ='mongodb+srv://codyjunier:Mastershake3!@cluster0.4bjws.mongodb.net/Big-Money?'
+// .env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
@@ -20,7 +21,8 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
